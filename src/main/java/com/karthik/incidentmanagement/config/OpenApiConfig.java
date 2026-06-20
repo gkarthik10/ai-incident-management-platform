f737f4d734b5c21eece888c7520d50a3f6450113
Karthik Kumar Reddy Guda <gudakarthikkumarreddy10@gmail.com>
2026-06-20 18:50:54 +0530

    Completed AI integration and Swagger security

--- src/main/java/com/karthik/incidentmanagement/config/OpenApiConfig.java
+++ src/main/java/com/karthik/incidentmanagement/config/OpenApiConfig.java
@@ -2,6 +2,7 @@ package com.karthik.incidentmanagement.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,12 +13,19 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenAPI() {
 
+        final String securitySchemeName = "bearerAuth";
+
         return new OpenAPI()
+                .addSecurityItem(
+                        new SecurityRequirement()
+                                .addList(securitySchemeName)
+                )
                 .components(
                         new Components()
                                 .addSecuritySchemes(
-                                        "bearerAuth",
+                                        securitySchemeName,
                                         new SecurityScheme()
+                                                .name(securitySchemeName)
                                                 .type(SecurityScheme.Type.HTTP)
                                                 .scheme("bearer")
                                                 .bearerFormat("JWT")
